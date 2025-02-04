0c28118f4c70568beddd4a7846d1e749327501fb
Waplll <106674268+Waplll@users.noreply.github.com>
2025-02-04 22:43:00 +0700

    4 prak

--- js/main.js
+++ js/main.js
@@ -12,6 +12,18 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 100,
-        onSale: true
+        onSale: true,
+        details: ['80% хлопок', '20% полиэстер', 'Гендерно-нейтральный'],
+        variants: [
+            {
+                variantsId: 2234,
+                variantColor: 'зелёные'
+            },
+            {
+                variantsId: 2235,
+                variantColor: 'синие'
+            }
+        ],
+        sizes: ['S','M','L','XL','XXL','XXXL']
     }
 })
