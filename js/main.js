fcc0c26c00ed9463721fbfed801fe4733d7594e4
Waplll <106674268+Waplll@users.noreply.github.com>
2025-02-04 22:24:42 +0700

    3 prak

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ let app = new Vue({
         description: "Пара теплых, пушистых носков.",
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "Пара носков",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true
     }
 })
