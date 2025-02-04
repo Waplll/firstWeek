608484d8066de828878f3f55c143e5172ce9c89f
Waplll <106674268+Waplll@users.noreply.github.com>
2025-02-04 22:11:58 +0700

    2 prak

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ let product = "Socks";
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        product: "Носки",
+        description: "Пара теплых, пушистых носков.",
+        image: "./img/vmSocks-green-onWhite.jpg",
+        altText: "Пара носков",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
