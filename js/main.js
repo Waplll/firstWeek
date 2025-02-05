5e5624a4eb1accf454be7660fafe9f71eac6d1dd
Waplll <106674268+Waplll@users.noreply.github.com>
2025-02-05 11:05:54 +0700

    7 prak

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Носки",
+        brand: 'Vue Mastery',
         description: "Пара теплых, пушистых носков.",
-        image: "./img/Green-Socks.jpg",
+        selectedVariant: 0,
         altText: "Пара носков",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
         inventory: 100,
         onSale: true,
         details: ['80% хлопок', '20% полиэстер', 'Гендерно-нейтральный'],
@@ -18,12 +18,14 @@ let app = new Vue({
             {
                 variantsId: 2234,
                 variantColor: 'green',
-                variantImage: "./img/Green-Socks.jpg"
+                variantImage: "./img/Green-Socks.jpg",
+                variantQuantity: 10
             },
             {
                 variantsId: 2235,
                 variantColor: 'blue',
-                variantImage: "./img/Blue-Socks.jpg"
+                variantImage: "./img/Blue-Socks.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['S','M','L','XL','XXL','XXXL'],
@@ -33,11 +35,30 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
         deleteFromCart() {
             this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            if (this.onSale) {
+                return "Распродажа!" + " " + this.brand + " " + this.product + " " + "по специальной цене!";
+            } else {
+                return "Распродажы нет." + " " + this.brand + " " + this.product + " " + "по стандартной цене.";
+            }
         }
     }
 })
